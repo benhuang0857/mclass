1a9087a7f22f7ca1817bf5fe2c015c87e258b9f2
Benhuang0857 <benhuang0857@gmail.com>
2025-12-06 02:16:46 +0800

    [Modify] Make Host url be correct

--- src/config/l5-swagger.php
+++ src/config/l5-swagger.php
@@ -312,7 +312,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://123.45.67.89/api'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://143.198.93.171/api'),
         ],
     ],
 ];
